bd0beb572e079c0569d7fd6aef181d1f132cf427
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-06-21 11:35:40 +0900

    create_controller_model

---
+++ db/migrate/20190621013305_create_groups.rb
@@ -0,0 +1,9 @@
+class CreateGroups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groups do |t|
+
+      t.timestamps
+      t.string :name, null: false, unique: true, index: true
+    end
+  end
+end
