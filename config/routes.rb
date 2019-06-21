a0103b29d6e894a5a4b192e5fb41aded6a17407b
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-06-21 20:05:07 +0900

    edit_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only:[:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
   end
 end
